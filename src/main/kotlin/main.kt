b9b9041721e71a70fe3f3e2434966e99d30fc5d0
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-23 21:04:09 +0400

    MutableStateFlow("")

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,9 @@
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -14,28 +17,36 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.flow.MutableStateFlow
 
-fun main() = application {
-    Window(
-        state = WindowState(size = DpSize(350.dp, 500.dp)),
-        onCloseRequest = ::exitApplication
-    ) {
-        Column(
-            modifier = Modifier.padding(50.dp)
+fun main() = run {
+    val textFlow = MutableStateFlow("")
+
+    application {
+        Window(
+            state = WindowState(size = DpSize(350.dp, 500.dp)),
+            onCloseRequest = ::exitApplication
         ) {
-            for (x in 1..5) {
-                val text = remember { mutableStateOf("") }
-                OutlinedTextField(
-                    value = text.value,
-                    onValueChange = { text.value = it },
-                    modifier = Modifier.moveFocusOnTab()
-                )
-                Spacer(Modifier.height(10.dp))
+            Column(
+                modifier = Modifier.padding(50.dp)
+            ) {
+                for (x in 1..5) {
+                    val textState = textFlow.collectAsState()
+                    val text = remember { textState }
+                    TextField(
+                        value = text.value,
+                        onValueChange = { textFlow.value = it },
+                        label = { Text("Test") },
+                        singleLine = true,
+                        modifier = Modifier.moveFocusOnTab()
+                            .fillMaxWidth()
+                    )
+                    Spacer(Modifier.height(10.dp))
+                }
             }
         }
     }
 }
-
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Modifier.moveFocusOnTab(
